1d36b0b0c2cbe3f45da989033d86b1ceb2c62cbd
Lorenzo Catalano <lor.catalano@gmail.com>
2022-01-13 14:37:26 +0100

    fixes

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "2.0.1"
 
 libraryDependencies += "com.typesafe.slick" %% "slick" % "3.3.3"
 libraryDependencies += "com.typesafe.slick" %% "slick-hikaricp" % "3.3.3"
+libraryDependencies += "com.softwaremill.sttp.client3" %% "core" % "3.3.17"
 
 Docker / packageName := "nodo-dei-pagamenti-mock"
 dockerBaseImage := "adoptopenjdk:11-jre-hotspot"
--- src/main/scala/it/lorenz/Server.scala
+++ src/main/scala/it/lorenz/Server.scala
@@ -4,62 +4,64 @@ import slick.jdbc.JdbcBackend.Database
 import slick.jdbc.OracleProfile
 import slick.util.AsyncExecutor
 
+import java.io.File
 import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Paths}
-import java.time.LocalDateTime
+import java.nio.file.{ Files, Paths }
 import java.util.Base64
+import java.util.concurrent.atomic.AtomicInteger
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.util.Try
 import scala.xml.XML
 
-
-object Server extends cask.MainRoutes{
+object Server extends cask.MainRoutes {
   override def port: Int = sys.env.get("MOCKPORT").map(_.toInt).getOrElse(8087)
   override def host: String = "0.0.0.0"
 
+  val basepath: String = new File(".").getCanonicalPath
+
+  var countChiediRT = new AtomicInteger(0)
+  var countAckRT = new AtomicInteger(0)
 
   val db = Database.forURL(
     "jdbc:oracle:thin:@//host.k3d.internal:1521/ORCLCDB.localdomain",
     driver = "oracle.jdbc.driver.OracleDriver",
-    user="NODO_ONLINE",
+    user = "NODO_ONLINE",
     password = "NODO_ONLINE",
     executor = AsyncExecutor("test1", minThreads = 10, queueSize = 1000, maxConnections = 10, maxThreads = 10)
   )
   val onlineRepository = OnlineRepository(OracleProfile, db)
 
-  def getNoticeNumberData(noticeNumber:String): (String, Option[Long], Option[Int], Option[Long]) ={
-    val auxDigit = noticeNumber.substring(0,1).toLong
+  def getNoticeNumberData(noticeNumber: String): (String, Option[Long], Option[Int], Option[Long]) = {
+    val auxDigit = noticeNumber.substring(0, 1).toLong
     val segregazione = auxDigit match {
-      case 3 | 4 => Some(noticeNumber.substring(1,3).toLong)
-      case _ => None
+      case 3 | 4 => Some(noticeNumber.substring(1, 3).toLong)
+      case _     => None
     }
     val progressivo = auxDigit match {
-      case 0 => Some(noticeNumber.substring(1,3).toInt)
+      case 0 => Some(noticeNumber.substring(1, 3).toInt)
       case _ => None
     }
     val auxValue = auxDigit.toLong match {
       case 0 | 3 => None
-      case _ => Option(auxDigit.toLong)
+      case _     => Option(auxDigit.toLong)
     }
-    val iuv = if(auxDigit==0){
+    val iuv = if (auxDigit == 0) {
       noticeNumber.substring(3)
-    }else{
+    } else {
       noticeNumber.substring(1)
     }
-    (iuv,segregazione,progressivo,auxValue)
+    (iuv, segregazione, progressivo, auxValue)
   }
-
 
   @cask.get("/")
   def hello() = {
-    println(s"/ ${LocalDateTime.now()}")
-    """{"name":"addios"}"""
+    s"""{"countChiediRT":"$countChiediRT","countAckRT":"$countAckRT"}"""
   }
 
   @cask.post("/", subpath = true)
   def doThing(request: cask.Request) = {
-
+    countChiediRT.incrementAndGet()
     Try({
       val payload = new String(request.readAllBytes())
       val xml = XML.loadString(payload)
@@ -68,46 +70,55 @@ object Server extends cask.MainRoutes{
 
       val primitiva = (xml \\ "Body" \ "_").head.label
 
-      val path = Paths.get(s"/mocks/${primitiva}/${tcid.getOrElse("OK")}.xml")
-      println(s"${LocalDateTime.now()} $primitiva(${action}) -> ${primitiva}/${tcid.getOrElse("OK")}.xml")
-      if(path.toFile.exists()){
+      val path = Paths.get(s"${basepath}/mocks/${primitiva}/${tcid.getOrElse("OK")}.xml")
+//      println(s"${LocalDateTime.now()} $primitiva(${action}) -> ${primitiva}/${tcid.getOrElse("OK")}.xml")
+      if (path.toFile.exists()) {
         val resString = new String(Files.readAllBytes(path), StandardCharsets.UTF_8)
         primitiva match {
           case "pspChiediListaRT" => {
 
             val canale = (xml \\ "identificativoCanale").text
-            val rpts = Await.result(onlineRepository.getForChiediLista(canale),Duration.Inf)
-            val sss = rpts.map(s=>{
+            val rpts = Await.result(onlineRepository.getForChiediLista(canale), Duration.Inf)
+
+            val sss = rpts.map(s => {
               <elementoListaRTResponse>
                 <identificativoDominio>{s.idDominio}</identificativoDominio>
                 <identificativoUnivocoVersamento>{s.iuv}</identificativoUnivocoVersamento>
                 <codiceContestoPagamento>{s.ccp}</codiceContestoPagamento>
               </elementoListaRTResponse>
             })
-            resString.replace("{ELEMENTI}",sss.mkString("\n"))
+
+            println(s"sending response pspChiediListaRT canale $canale")
+            resString.replace("{ELEMENTI}", sss.mkString("\n"))
           }
 
           case "pspChiediRT" =>
-            val dom =(xml \\ "identificativoDominio").text
+            val dom = (xml \\ "identificativoDominio").text
             val iuv = (xml \\ "identificativoUnivocoVersamento").text
             val ccp = (xml \\ "codiceContestoPagamento").text
-            val pathrt = Paths.get(s"/mocks/pspChiediRT/rt.xml")
+            val pathrt = Paths.get(s"$basepath/mocks/pspChiediRT/rt.xml")
             val rt = new String(Files.readAllBytes(pathrt), StandardCharsets.UTF_8)
-            val rtrep = rt
-              .replace("{pa}",dom)
-              .replace("{iuv}",iuv)
-              .replace("{ccp}",ccp)
-            resString.replace("{RT}",Base64.getEncoder.encodeToString(rtrep.getBytes))
+            val rtrep = rt.replace("{pa}", dom).replace("{iuv}", iuv).replace("{ccp}", ccp)
+            val c = countChiediRT.incrementAndGet()
+            val canale = request.headers.get("canale").flatMap(_.headOption)
+//            println(s"sending response pspChiediRT canale:${canale.getOrElse("n/d")} $c $iuv")
+            println(s"insert into SERVER (iuv,messagetype) values ('$iuv','pspChiediRT');")
+            resString.replace("{RT}", Base64.getEncoder.encodeToString(rtrep.getBytes))
 
           case "paGetPaymentReq" =>
-            val nn =  (xml \\ "noticeNumber").text
-            val (iuv,_,_,_) = getNoticeNumberData(nn)
-            resString.replace("{CREDITORREFERENCEID}",iuv)
+            val nn = (xml \\ "noticeNumber").text
+            val (iuv, _, _, _) = getNoticeNumberData(nn)
+            resString.replace("{CREDITORREFERENCEID}", iuv)
 
-          case _ =>
+          case r =>
+            if (r == "pspInviaAckRT") {
+              countAckRT.incrementAndGet()
+              val iuv = (xml \\ "identificativoUnivocoVersamento").text
+              println(s"insert into SERVER (iuv,messagetype) values ('$iuv','pspChiediRT');")
+            }
             resString
         }
-      }else{
+      } else {
         println(s"not found ${path}")
         s"not found ${path}"
       }
